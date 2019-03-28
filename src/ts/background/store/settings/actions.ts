ba3b5feebd85cfce7837c70c920578da1062b463
tonytao <taotao8851@gmail.com>
2019-03-28 22:12:38 +1100

    remove unused const

--- src/ts/background/store/settings/actions.ts
+++ src/ts/background/store/settings/actions.ts
@@ -3,6 +3,3 @@ import { Action } from 'redux'
 export type ThemeActionTypes = 'DARK_THEME' | 'LIGHT_THEME'
 
 export type SettingsActions = Action<ThemeActionTypes>
-
-export const setDarkTheme = () => ({ type: 'DARK_THEME' })
-export const setLightTheme = () => ({ type: 'LIGHT_THEME' })
