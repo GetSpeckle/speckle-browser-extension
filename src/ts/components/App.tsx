3ad4a4f6571f66a3bf6c1c6f99c1fe5f328c77a1
tonytao <taotao8851@gmail.com>
2019-04-02 23:10:46 +1100

    move logic to app container and keep only render logic in app component

--- src/ts/components/App.tsx
+++ src/ts/components/App.tsx
@@ -3,19 +3,18 @@ import * as React from 'react'
 import { Image, Button } from 'semantic-ui-react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import { IAppState } from '../background/store/all'
-import { connect } from 'react-redux'
-import { changeColor } from '../background/store/settings'
 import { TERM_SERVICE_ROUTE } from '../constants/routes'
 
-interface IHomeProp extends StateProps, DispatchProps {}
+interface IHomeProp {
+  changeColor: (color: string) => any,
+  color: string
+}
 
 interface IHomeState {
   color: string
 }
 
-class Home extends React.Component<IHomeProp, IHomeState> {
-
+export default class App extends React.Component<IHomeProp, IHomeState> {
   state: IHomeState = {
     color: 'red'
   }
@@ -33,7 +32,7 @@ class Home extends React.Component<IHomeProp, IHomeState> {
           <div>
             {t('welcome')}
             <div>Current color: {this.props.color}</div>
-            <Button onClick={this.handleChangeColor}>Chage color to red</Button>
+            <Button onClick={this.handleChangeColor}>Change color to red</Button>
           </div>
           <Link to={TERM_SERVICE_ROUTE}>Term</Link>
         </LandingContainer>
@@ -50,15 +49,3 @@ const LandingContainer = styled('div')`
     margin: 5px;
     background-color: ${p => p.theme.backgroundColor};
 `
-const mapStateToProps = (state: IAppState) => {
-  return {
-    color: state.settings.color
-  }
-}
-
-type StateProps = ReturnType<typeof mapStateToProps>
-type DispatchProps = typeof mapDispatchToProps
-
-const mapDispatchToProps = { changeColor }
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
