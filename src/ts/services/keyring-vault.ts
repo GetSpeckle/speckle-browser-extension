b84aabf6baa1c2f1475f3aa37f7144d5077f514e
fyang1024 <fyang1024@gmail.com>
2019-04-08 22:56:09 +1000

    Fix issues according to reviews

--- src/ts/services/keyring-vault.ts
+++ src/ts/services/keyring-vault.ts
@@ -5,9 +5,10 @@ import { LocalStore } from './local-store'
 import { mnemonicGenerate, cryptoWaitReady, mnemonicValidate } from '@polkadot/util-crypto'
 import t from './i18n'
 
+const VAULT_KEY: string = 'speckle-vault'
+
 class KeyringVault {
 
-  private _vaultKey: string = 'speckle-vault'
   private _keyring?: KeyringInstance
   private _password?: string
   private _mnemonic?: string
@@ -20,14 +21,18 @@ class KeyringVault {
   }
 
   unlock (password: string, addressPrefix?: Prefix): Promise<Array<KeyringPair$Json>> {
-    if (this.isUnlocked()) throw new Error(t('error.wallet.unlocked'))
+    if (this.isUnlocked()) {
+      return new Promise<Array<KeyringPair$Json>>(() => this.keyring.getPairs().map((pair) => {
+        pair.toJson(this._password)
+      }))
+    }
     if (!password.length) throw new Error(t('error.password'))
     // this will be redundant if we have polkadot js api initialisation
     return cryptoWaitReady().then(async () => {
       this._keyring = new Keyring({ addressPrefix, type: 'sr25519' })
-      let vault = await LocalStore.get(this._vaultKey)
+      let vault = await LocalStore.get(VAULT_KEY)
       if (vault) {
-        let accounts = Object.values(vault[this._vaultKey])
+        let accounts = Object.values(vault[VAULT_KEY])
         try {
           accounts.forEach((account) => {
             let pair = this.keyring.addFromJson(account as KeyringPair$Json)
@@ -76,33 +81,25 @@ class KeyringVault {
     if (this._mnemonic !== mnemonic) throw new Error(t('error.mnemonic.unmatched'))
     let pair = this.keyring.addFromMnemonic(mnemonic, { name: accountName })
     this._mnemonic = undefined
-    return this.saveAccount(pair).then((keyringPair$Json: KeyringPair$Json) => {
-      return keyringPair$Json
-    })
+    return this.saveAccount(pair)
   }
 
   updateAccountName (address: string, accountName: string): Promise<KeyringPair$Json> {
     if (this.isLocked()) throw new Error(t('error.wallet.locked'))
     const pair = this.keyring.getPair(address)
     if (!pair) throw new Error(t('error.account.notFound'))
-    let meta = pair.getMeta()
-    meta.name = accountName
-    pair.setMeta(meta)
-    this.keyring.addPair(pair)
-    return this.saveAccount(pair).then((json) => {
-      return json
-    })
+    pair.setMeta({ ...pair.getMeta(), name: accountName })
+    return this.saveAccount(pair)
   }
 
   removeAccount (address: string) {
     if (this.isLocked()) throw new Error(t('error.wallet.locked'))
     this.keyring.removePair(address)
-    LocalStore.get(this._vaultKey).then((vault) => {
-      if (vault && vault[this._vaultKey]) {
-        delete vault[this._vaultKey][address]
-        return LocalStore.set(vault[this._vaultKey])
+    LocalStore.get(VAULT_KEY).then(async (vault) => {
+      if (vault && vault[VAULT_KEY]) {
+        delete vault[VAULT_KEY][address]
+        await LocalStore.set(vault[VAULT_KEY])
       }
-      return
     })
   }
 
@@ -114,25 +111,17 @@ class KeyringVault {
     if (this.isLocked()) throw new Error(t('error.wallet.locked'))
     if (!this.isMnemonicValid(mnemonic)) throw new Error(t('error.mnemonic.invalid'))
     let pair = this.keyring.createFromUri(mnemonic, { name: accountName, imported: true })
-    this.keyring.addPair(pair)
-    return this.saveAccount(pair).then((keyringPair$Json: KeyringPair$Json) => {
-      return keyringPair$Json
-    })
+    return this.saveAccount(pair)
   }
 
   importAccountFromJson (json: KeyringPair$Json, password?: string): Promise<KeyringPair$Json> {
     if (this.isLocked()) throw new Error(t('error.wallet.locked'))
     let pair = this.keyring.addFromJson(json)
-    let meta = pair.getMeta()
-    meta.imported = true
-    pair.setMeta(meta)
+    pair.setMeta({ ...pair.getMeta(), imported: true })
     if (password) {
       pair.decodePkcs8(password)
     }
-    this.keyring.addPair(pair)
-    return this.saveAccount(pair).then((json) => {
-      return json
-    })
+    return this.saveAccount(pair)
   }
 
   isLocked (): boolean {
@@ -145,16 +134,17 @@ class KeyringVault {
 
   private saveAccount (pair: KeyringPair): Promise<KeyringPair$Json> {
     this.addTimestamp(pair)
-    const json = pair.toJson(this._password)
-    return LocalStore.get(this._vaultKey).then((vault: any) => {
+    const keyringPair$Json: KeyringPair$Json = pair.toJson(this._password)
+    return LocalStore.get(VAULT_KEY).then((vault) => {
       if (!vault) {
         vault = {}
       } else {
-        vault = vault[this._vaultKey]
+        vault = vault[VAULT_KEY]
       }
-      vault[json.address] = json
-      return LocalStore.setValue(this._vaultKey, vault).then(() => {
-        return json
+      vault[keyringPair$Json.address] = keyringPair$Json
+      return LocalStore.setValue(VAULT_KEY, vault).then(() => {
+        this.keyring.addPair(pair)
+        return keyringPair$Json
       })
     })
   }
