72e78c3c30c988c85c2c5a8c67630c609800323a
Chao Wang <chwang7@gmail.com>
2019-04-09 22:00:09 +1000

    ui update

--- src/ts/components/account/CreatePassword.tsx
+++ src/ts/components/account/CreatePassword.tsx
@@ -5,6 +5,10 @@ import Progress from './Progress'
 
 class CreatePassword extends React.Component {
 
+  handleClick () {
+    // TODO: create account
+  }
+
   render () {
     return (
         <div>
@@ -12,13 +16,19 @@ class CreatePassword extends React.Component {
           <Text>
             {t('passwordDescription')}
           </Text>
-          <div className='ui input'>
-            <input type='password' placeholder='Create new password' />
-          </div>
+          <Text>
+            <StyledPassword type='password' placeholder={t('Create new password')} />
+          </Text>
 
-          <div className='ui input'>
-            <input type='password' placeholder='Repeat password' />
-          </div>
+          <Text>
+            <StyledPassword type='password' placeholder={t('Repeat password')} />
+          </Text>
+
+          <Text>
+            <StyledButton onClick={this.handleClick}>
+              {t('Create Account')}
+            </StyledButton>
+          </Text>
 
         </div>
     )
@@ -36,7 +46,28 @@ const Text = styled.p`
     font-stretch: normal;
     line-height: normal;
     letter-spacing: normal;
-    text-align: center;
     color: #3e5860;
 `
+
+const StyledPassword = styled.input`
+  width: 311px;
+  height: 42px;
+`
+const StyledButton = styled.button`
+  width: 311px;
+  height: 45px;
+  border-radius: 4px;
+  box-shadow: 0 3px 10px 0 rgba(72, 178, 228, 0.21);
+  background-color: #24b6e8;
+  font-family: Nunito;
+  font-size: 16px;
+  font-weight: 800;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.31;
+  letter-spacing: normal;
+  text-align: center;
+  color: #ffffff;
+`
+
 export default CreatePassword
