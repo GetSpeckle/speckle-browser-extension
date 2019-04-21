802903241989ef4d30d6d7dae03dbde9f6f4b858
fyang1024 <fyang1024@gmail.com>
2019-04-21 21:06:52 +1000

    Fix tslint issue

--- src/ts/components/account/LoginFooter.tsx
+++ src/ts/components/account/LoginFooter.tsx
@@ -7,8 +7,7 @@ export default () => {
   return (
     <LoginFooter>
         <span>
-          <FooterLink href='https://speckleos.github.io/speckle-docs/'
-                      target='_blank'>
+          <FooterLink href='https://speckleos.github.io/speckle-docs/' target='_blank'>
             {t('help')}
           </FooterLink>
         </span>
