ef4a672e1bc43f0570c89d5d987d2d4b7f6dcd8d
fyang1024 <fyang1024@gmail.com>
2019-04-28 12:03:58 +1000

    Get account balance

--- jest.config.js
+++ jest.config.js
@@ -183,4 +183,6 @@ module.exports = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+
+  setupFilesAfterEnv: ['./jest.setup.js']
 };
---
+++ src/ts/background/services/account-balance.ts
@@ -0,0 +1,29 @@
+import { ApiPromise } from '@polkadot/api'
+import { WsProvider } from '@polkadot/rpc-provider'
+import { Network } from '../../constants/networks'
+import { ChainProperties } from '@polkadot/types'
+import { formatBalance } from '@polkadot/util'
+
+export async function getBalance (
+  address: string,
+  network: Network,
+  withFormat?: boolean
+): Promise<Number | string> {
+  const provider = new WsProvider(network.rpcServer)
+  try {
+    const api = await ApiPromise.create(provider)
+    const balance = await api.query.balances.freeBalance(address)
+    if (!withFormat) return Number(balance.toString())
+    const properties = await api.rpc.system.properties()
+    const chainProperties = (properties as ChainProperties)
+    formatBalance.setDefaults({
+      decimals: chainProperties.tokenDecimals,
+      unit: chainProperties.tokenSymbol
+    })
+    return formatBalance(balance.toString())
+  } catch (e) {
+    return 'N/A'
+  } finally {
+    if (provider.isConnected()) provider.disconnect()
+  }
+}
