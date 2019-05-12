0bb962773279e5cecc445bfa6647ea45a835c97f
hskang9 <hskang9@gmail.com>
2019-05-13 01:03:31 +0900

    Header component with color scheme

---
+++ src/ts/constants/color.ts
@@ -0,0 +1,29 @@
+// Color for Header
+
+export const color = {
+  'blue': {
+    inner: '#44C5EE', // inner circle
+    outer: '#4AABE0', // outer circle
+    shadow: '0 0 0 0 0.223431156 0 0 0 0 0.734036715 0 0 0 0 0.974888393 0 0 0 0.601704031 0'
+  },
+  'green': {
+    inner: '#51DFB0',
+    outer: '#52C48D',
+    shadow: '0 0 0 0 0.317647059 0 0 0 0 0.847058824 0 0 0 0 0.654901961 0 0 0 0.597599638 0'
+  },
+  'orange': {
+    inner: '#FFC10B',
+    outer: '#FFB200',
+    shadow: '0 0 0 0 1 0 0 0 0 0.698039216 0 0 0 0 0 0 0 0 0.6 0'
+  },
+  'purple': {
+    inner: '#D396FF',
+    outer: '#B279F9',
+    shadow: '0 0 0 0 0.698039216 0 0 0 0 0.474509804 0 0 0 0 0.976470588 0 0 0 0.6 0'
+  },
+  'red': {
+    inner: '#FF7396', // inner part
+    outer: '#F3536D', // outer part
+    shadow: '0 0 0 0 0.952941176 0 0 0 0 0.325490196 0 0 0 0 0.42745098 0 0 0 0.5 0'
+  }
+}
