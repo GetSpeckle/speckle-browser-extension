c0dd98107ab838b5e1361f2a21605f1a6ad0698f
fyang1024 <fyang1024@gmail.com>
2019-05-24 00:17:29 +1000

    Fix progress icon and refactoring

--- src/ts/layouts/LoginLayout.tsx
+++ src/ts/layouts/LoginLayout.tsx
@@ -1,11 +1,11 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {IAppState} from '../background/store/all'
-import {LayoutContainer} from '../components/basic-components'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { IAppState } from '../background/store/all'
+import { LayoutContainer } from '../components/basic-components'
 import ErrorMessage from '../components/error/ErrorMessage'
 import LoginFooter from '../components/account/LoginFooter'
 import Header from './Header'
-import {color} from '../constants/color'
+import { colorSchemes } from '../components/styles/themes'
 
 interface ILoginLayoutProps extends StateProps, DispatchProps {
 }
@@ -14,7 +14,7 @@ class LoginLayout extends Component<ILoginLayoutProps> {
   render () {
     return (
       <LayoutContainer>
-        <Header color={color[this.props.settings.color]}/>
+        <Header colorScheme={colorSchemes[this.props.settings.color]}/>
         <ErrorMessage message={this.props.error} style={alignMiddle}/>
         {this.props.children}
         <LoginFooter/>
