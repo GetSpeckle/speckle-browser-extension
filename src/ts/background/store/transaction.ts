a49a26ac831ccb76381a570cab66e59df418aa94
Chao Wang <chwang7@gmail.com>
2019-05-24 00:12:52 +1000

    add transaction action functions

--- src/ts/background/store/transaction.ts
+++ src/ts/background/store/transaction.ts
@@ -1,7 +1,12 @@
+import { LocalStore } from '../../services/local-store';
+import { AnyAction } from 'redux';
 
 export type TransactionType = 'Sent' | 'Received' | 'Staked'
 export type TransactionStatus = 'Pending' | 'Success' | 'Failure'
 
+/**
+ * Transaction model
+ */
 export interface ITransaction {
   id: string // uuid
   from: string
@@ -13,4 +18,36 @@ export interface ITransaction {
   txHash?: string
   createTime: Date
   updateTime?: Date
+}
+
+/**
+ * Map of account address -> transaction list
+ */
+export interface IAccountTransactions {
+  [index: string]: ITransaction[]
+}
+
+export const ACTION_TYPES = {
+  GET_TRANSACTIONS: 'GET_TRANSACTIONS',
+  SAVE_TRNASACTIONS: 'SAVE_TRNASACTIONS',
+  ADD_TRNASACTION: 'ADD_TRNASACTION'
+}
+
+const PREFIX = 'transactions_'
+
+export function getTransactions (address: string): AnyAction {
+  return {
+    type: ACTION_TYPES.GET_TRANSACTIONS,
+    payload: LocalStore.get(PREFIX + address)
+  }
+}
+
+export function addTransaction (address: string,
+    tran: ITransaction, list: ITransaction[]): AnyAction {
+  // TODO: make some validation
+  list.unshift(tran)
+  return {
+    type: ACTION_TYPES.ADD_TRNASACTION,
+    payload: LocalStore.setValue(PREFIX + address, list)
+  }
 }
