8423cb90d98710e8cae99272a8d409968d82e942
Chao Wang <chwang7@gmail.com>
2019-05-24 23:06:07 +1000

    transactions reducer and  state

--- src/ts/background/store/all.ts
+++ src/ts/background/store/all.ts
@@ -3,16 +3,18 @@ import settings, { IAppSettings } from './settings/reducer'
 import wallet, { IWallet } from './wallet'
 import error, { IError } from './error'
 import apiContext, { IApiContext } from './api-context/reducer'
+import transactions, { IAccountTransactions } from './transaction'
 
 export interface IAppState {
   settings: IAppSettings
   wallet: IWallet
   apiContext: IApiContext
   error: IError
+  transactions: IAccountTransactions
 }
 
 const reducers = combineReducers<IAppState>({
-  settings, wallet, apiContext, error
+  settings, wallet, apiContext, error, transactions
 })
 
 export default reducers
--- src/ts/background/store/transaction.ts
+++ src/ts/background/store/transaction.ts
@@ -1,5 +1,6 @@
-import { LocalStore } from '../../services/local-store';
-import { AnyAction } from 'redux';
+import { LocalStore } from '../../services/local-store'
+import { AnyAction, Reducer } from 'redux'
+import { SUCCESS } from './util'
 
 export type TransactionType = 'Sent' | 'Received' | 'Staked'
 export type TransactionStatus = 'Pending' | 'Success' | 'Failure'
@@ -27,6 +28,8 @@ export interface IAccountTransactions {
   [index: string]: ITransaction[]
 }
 
+const initialState: IAccountTransactions = {}
+
 export const ACTION_TYPES = {
   GET_TRANSACTIONS: 'GET_TRANSACTIONS',
   SAVE_TRNASACTIONS: 'SAVE_TRNASACTIONS',
@@ -38,6 +41,7 @@ const PREFIX = 'transactions_'
 export function getTransactions (address: string): AnyAction {
   return {
     type: ACTION_TYPES.GET_TRANSACTIONS,
+    address: address,
     payload: LocalStore.get(PREFIX + address)
   }
 }
@@ -48,6 +52,44 @@ export function addTransaction (address: string,
   list.unshift(tran)
   return {
     type: ACTION_TYPES.ADD_TRNASACTION,
+    address: address,
     payload: LocalStore.setValue(PREFIX + address, list)
   }
 }
+
+export function saveTransactions (address: string, list: ITransaction[]): AnyAction {
+
+  return {
+    type: ACTION_TYPES.SAVE_TRNASACTIONS,
+    address: address,
+    payload: LocalStore.setValue(PREFIX + address, list)
+  }
+}
+
+/**
+ * transaction reducer
+ *
+ * @param state the current state
+ * @param action the action received
+ */
+const transactions: Reducer<IAccountTransactions, AnyAction> = (state = initialState, action) => {
+  switch (action.type) {
+
+    case SUCCESS(ACTION_TYPES.ADD_TRNASACTION):
+      console.log('added transaction', action.payload)
+      return { ...state, [action.address]: action.payload }
+
+    case SUCCESS(ACTION_TYPES.GET_TRANSACTIONS):
+      console.log('got the transactions', action.payload)
+      return { ...state, [action.address]: action.payload }
+
+    case SUCCESS(ACTION_TYPES.SAVE_TRNASACTIONS):
+      console.log('saved the transactions', action.payload)
+      return { ...state, [action.address]: action.payload }
+
+    default:
+      return state
+  }
+}
+
+export default transactions
