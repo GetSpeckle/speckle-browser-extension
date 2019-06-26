a7d0a5762b1b6f755b9beb7d34029b4287759f69
hskang9 <hskang9@gmail.com>
2019-06-27 05:53:24 +0900

    Refactor Confirmation

--- src/ts/components/transaction/Confirm.tsx
+++ src/ts/components/transaction/Confirm.tsx
@@ -78,104 +78,117 @@ export default class Confirm extends React.Component<IConfirmProps, IConfirmStat
     if (doesNotExist) {
       warning = (
         <Warning>
-        <div>
-          <Icon name='warning sign' size={'small'}/>
-          The final recipient balance is less or equal
-          to {formatBalance(this.props.existentialDeposit)} (the existential amount) and will
-          not be reflected
-        </div>
-        <div>
-          <Icon name='warning sign' size={'small'}/>
-          A fee of {formatBalance(this.props.creationFee)} will be deducted from the sender
-          since the destination account does not exist
-        </div>
-      </Warning>
+          <div>
+            <Icon name='warning sign' size={'small'}/>
+            The final recipient balance is less or equal
+            to {formatBalance(this.props.existentialDeposit)} (the existential amount) and will
+            not be reflected
+          </div>
+          <div>
+            <Icon name='warning sign' size={'small'}/>
+            A fee of {formatBalance(this.props.creationFee)} will be deducted from the sender
+            since the destination account does not exist
+          </div>
+        </Warning>
       )
     } else {
       warning = null
     }
 
     return (
-    <Modal
-      trigger={this.props.trigger}
-      style={{ 'zIndex': 3 }}
-      onClose={this.handleClose}
-      open={this.props.open}
-    >
-      <UpperSection>
-        <Offset>
-          <Status>
-            <Identicon account={this.props.fromAddress} size={48}/>
-          </Status>
-        </Offset>
-        <Heading>Confirm Extrinsic</Heading>
-        <Subheading>Review your extrinsic details</Subheading>
-      </UpperSection>
-      <OverlaySection>
-        <Overlay/>
-      </OverlaySection>
-      <Section>
-        <FromTo color={colorSchemes[this.props.color].backgroundColor}>
-          <Icon name='arrow circle right' size={'big'} style={{  'marginLeft': '10px' }}/>
-          <Container textAlign={'left'} style={{ 'marginLeft': '10px' }}>
-            <Tooltip
-              title={!this.state.message ? t('copyToClipboard') : t('copiedExclam')}
-              position='bottom'
-              trigger='mouseenter'
-              arrow={true}
-            >
-              <span onClick={() => this.copyToClipboard(this.props.toAddress)}>{this.truncate(this.props.toAddress)}</span>
-            </Tooltip>
-            <div style={{ 'display': 'flex', 'alignItems': 'center', 'fontSize': '13px' }}>
-              <Identicon account={this.props.toAddress} size={15}/>
-              <p>  {formatBalance(this.props.recipientAvailable)}</p>
-            </div>
-          </Container>
-        </FromTo>
-      </Section>
-      <Section>
-        <Info>
-          <Key>Fee</Key>
-          <Value>{formatBalance(this.props.fee)}</Value>
-        </Info>
-      </Section>
-      <Section>
-        <Info>
-          <Key>Amount</Key>
-          <Value>{formatBalance(this.props.amount)}</Value>
-        </Info>
-      </Section>
-      <Section>
-        <Info>
-          <Key>total</Key>
-          <Value>{formatBalance(this.props.amount.add(this.props.fee))}</Value>
-        </Info>
-      </Section>
-      <Section>
-        <Info>
-          {warning}
-        </Info>
-        <Info>
-          <div style={{ 'fontSize': '11px' }}>
-            <div>
-              <Icon name='arrow right' size={'small'}/>
-              Fees include the transaction fee and the per-byte fee
-            </div>
-          </div>
-        </Info>
-      </Section>
-      <Section style={{ 'marginTop': '20px' }}>
-        <Info>
-          <Button onClick={this.handleClose}>Cancel</Button>
-          <ConfirmButton
-            color={colorSchemes[this.props.color].backgroundColor}
-            onClick={this.handleConfirm}
+      <Modal
+        trigger={this.props.trigger}
+        style={{ 'zIndex': 3 }}
+        onClose={this.handleClose}
+        open={this.props.open}
+      >
+        <UpperSection>
+          <Offset>
+            <Status>
+              <Identicon account={this.props.fromAddress} size={48}/>
+            </Status>
+          </Offset>
+          <Tooltip
+            title={!this.state.message ? t('copyToClipboard') : t('copiedExclam')}
+            position='bottom'
+            trigger='mouseenter'
+            arrow={true}
           >
-            Confirm
-          </ConfirmButton>
-        </Info>
-      </Section>
-    </Modal>
+            <FromAddress>
+              {this.props.fromAddress.substring(0, 8) + '...' + this.props.fromAddress.substring(this.props.fromAddress.length - 10)}
+            </FromAddress>
+          </Tooltip>
+          <Heading>Confirm Extrinsic</Heading>
+          <Subheading>Review your extrinsic details</Subheading>
+        </UpperSection>
+        <OverlaySection>
+          <Overlay/>
+        </OverlaySection>
+        <Section>
+          <FromTo color={colorSchemes[this.props.color].backgroundColor}>
+            <Icon name='arrow circle right' size={'big'} style={{  'marginLeft': '10px' }}/>
+            <Container textAlign={'left'} style={{ 'marginLeft': '10px' }}>
+              <div style={{ 'display': 'flex', 'alignItems': 'center', 'fontSize': '13px', 'marginLeft': '-10px' }}>
+                <Identicon account={this.props.toAddress} size={20} style={{ 'marginRight': '5px' }}/>
+                <Tooltip
+                  title={!this.state.message ? t('copyToClipboard') : t('copiedExclam')}
+                  position='bottom'
+                  trigger='mouseenter'
+                  arrow={true}
+                >
+                  <span onClick={() => this.copyToClipboard(this.props.toAddress)}>{this.truncate(this.props.toAddress)}</span>
+                </Tooltip>
+              </div>
+              <div style={{ 'display': 'flex', 'alignItems': 'center', 'fontSize': '11px', 'marginLeft': '5px' }}>
+                <p>Available: {formatBalance(this.props.recipientAvailable)}</p>
+              </div>
+            </Container>
+          </FromTo>
+        </Section>
+        <Section>
+          <Info>
+            <Key>Fee</Key>
+            <Value>{formatBalance(this.props.fee)}</Value>
+          </Info>
+          <div style={{ 'border': '1px solid gray' }}/>
+        </Section>
+        <Section>
+          <Info>
+            <Key>Amount</Key>
+            <Value>{formatBalance(this.props.amount)}</Value>
+          </Info>
+        </Section>
+        <Section>
+          <Info>
+            <Key>total</Key>
+            <Value>{formatBalance(this.props.amount.add(this.props.fee))}</Value>
+          </Info>
+        </Section>
+        <Section>
+          <Info>
+            {warning}
+          </Info>
+          <Info>
+            <div style={{ 'fontSize': '11px' }}>
+              <div>
+                <Icon name='arrow right' size={'small'}/>
+                Fees include the transaction fee and the per-byte fee
+              </div>
+            </div>
+          </Info>
+        </Section>
+        <Section style={{ 'marginTop': '20px' }}>
+          <Info>
+            <Button onClick={this.handleClose}>Cancel</Button>
+            <ConfirmButton
+              color={colorSchemes[this.props.color].backgroundColor}
+              onClick={this.handleConfirm}
+            >
+              Confirm
+            </ConfirmButton>
+          </Info>
+        </Section>
+      </Modal>
     )
   }
 }
@@ -190,10 +203,11 @@ width: 100%
 margin-top:-34px;
 display: flex;
 justify-content: center;
+margin-bottom: 8px;
 `
 const Heading = styled.h3`
 {
-  margin-top: 20px;
+  margin-top: 8px;
   margin-bottom: 4px;
   font-family: Nunito;
   font-size: 19px;
@@ -221,7 +235,7 @@ const Subheading = styled.p`
 
 const OverlaySection = styled.div`
   width: 100%
-  margin-top: 18px
+  margin-top: 10px
   text-align: center
 `
 
@@ -240,6 +254,16 @@ const Status = styled.div`
 const Key = styled.p`
   font-family: Nunito;
   font-size: 11px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #a0aeb4;
+`
+const FromAddress = styled.span`
+  font-family: Nunito;
+  font-size: 8px;
   font-weight: bold;
   font-style: normal;
   font-stretch: normal;
@@ -275,8 +299,8 @@ const FromTo = styled.div`
   align-items: center;
   background-color: ${ props => props.color }
   color: #fff;
-  margin-top: 10px;
-  margin: 10px;
+  margin-top: 15px;
+  margin-left: 10px;
  `
 
 const Info = styled.div`
