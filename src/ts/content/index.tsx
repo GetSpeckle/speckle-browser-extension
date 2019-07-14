5d8554449f8e0cb8e84cc1755fdf2dc902d82932
fyang1024 <fyang1024@gmail.com>
2019-07-14 21:41:54 +1000

    Refactoring

--- src/ts/content/index.tsx
+++ src/ts/content/index.tsx
@@ -3,45 +3,15 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import SpeckleApp from './containers/SpeckleApp'
 
-import { createDomAnchor } from '../scripts/dom'
+import { createDomAnchor, createPageScript } from '../scripts/dom'
 import { store } from '../background/store'
-import extension from 'extensionizer'
-
-import { PORT_CONTENT } from '../constants/ports'
-import { ORIGIN_CONTENT, ORIGIN_PAGE } from '../constants/origins'
+import { setupPort } from './port'
 
 createDomAnchor('speckle-root')
 
 ReactDOM.render(<Provider store={store}><SpeckleApp/></Provider>,
   document.getElementById('speckle-root'))
 
-// connect to the extension
-const port = extension.runtime.connect({ name: PORT_CONTENT })
+setupPort()
 
-// send any messages from the extension back to the page
-port.onMessage.addListener((data) => {
-  window.postMessage({ ...data, origin: ORIGIN_CONTENT }, '*')
-})
-
-// all messages from the page, pass them to the extension
-window.addEventListener('message', ({ data, source }) => {
-  // only allow messages from our window, by the inject
-  if (source !== window || data.origin !== ORIGIN_PAGE) {
-    return
-  }
-
-  port.postMessage(data)
-})
-
-// inject our data injector
-const script = document.createElement('script')
-
-script.src = extension.extension.getURL('speckle.js')
-script.onload = (): void => {
-  // remove the injecting tag when loaded
-  if (script.parentNode) {
-    script.parentNode.removeChild(script)
-  }
-}
-
-(document.head || document.documentElement).appendChild(script)
+createPageScript()
--- src/ts/scripts/dom.ts
+++ src/ts/scripts/dom.ts
@@ -1,5 +1,22 @@
+import extension from 'extensionizer'
+
 export const createDomAnchor = (anchorId: string) => {
   const anchor = document.createElement('div')
   anchor.id = anchorId
   document.body.insertBefore(anchor, document.body.childNodes[0])
+}
+
+export const createPageScript = () => {
+  // inject our data injector
+  const script = document.createElement('script')
+
+  script.src = extension.extension.getURL('speckle.js')
+  script.onload = (): void => {
+    // remove the injecting tag when loaded
+    if (script.parentNode) {
+      script.parentNode.removeChild(script)
+    }
+  }
+
+  (document.head || document.documentElement).appendChild(script)
 }
