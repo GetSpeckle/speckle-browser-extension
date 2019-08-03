effcdb70c43d4925a62d0e75f72f98baadf4bfe0
Chao Wang <chwang7@gmail.com>
2019-08-03 21:48:10 +1000

    alert panel styling

--- src/ts/components/page/AlertPanel.tsx
+++ src/ts/components/page/AlertPanel.tsx
@@ -1,15 +1,7 @@
 import * as React from 'react'
-import { withRouter, RouteComponentProps } from 'react-router'
-import { Menu, Icon, Message } from 'semantic-ui-react'
-import t from '../../services/i18n'
-import { HOME_ROUTE, SEND_ROUTE } from '../../constants/routes'
-import { IAppState } from '../../background/store/all'
-import { connect } from 'react-redux'
-import { SemanticCOLORS } from 'semantic-ui-react/dist/commonjs/generic'
+import { Icon, Message } from 'semantic-ui-react'
 
-type StateProps = ReturnType<typeof mapStateToProps>
-
-interface IAlertPanelProps extends StateProps, RouteComponentProps {
+interface IAlertPanelProps {
   message: string
   icon: 'linkify' | 'exclamation'
   color: string
@@ -21,9 +13,16 @@ interface IAlertPanelProps extends StateProps, RouteComponentProps {
 class AlertPanel extends React.Component<IAlertPanelProps> {
 
   render () {
+    const borderStyle = {
+      borderLeftColor: this.props.color,
+      borderLeftWidth: '2px',
+      borderLeftStyle: 'solid',
+      borderRadius: '2px'
+    }
+
     return (
       <div>
-        <Message icon={true}>
+        <Message icon={true} className='alert' style={borderStyle}>
           <Icon name={this.props.icon} />
           <Message.Content>
             {this.props.message}
@@ -34,10 +33,4 @@ class AlertPanel extends React.Component<IAlertPanelProps> {
   }
 }
 
-const mapStateToProps = (state: IAppState) => {
-  return {
-    settings: state.settings
-  }
-}
-
-export default withRouter(connect(mapStateToProps)(AlertPanel))
+export default AlertPanel
