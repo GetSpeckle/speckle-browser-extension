4e99705c4c0a8e4f27ed2f40cf9cd67d4ea3cb4e
fyang1024 <fyang1024@gmail.com>
2019-09-09 00:08:46 +1000

    Upgrade api libs to the latest

--- src/ts/background/types.ts
+++ src/ts/background/types.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { SignerPayload } from '@polkadot/api/types'
+import { SignerPayloadJSON } from '@polkadot/types/types'
 
 export type MessageTypes = 'authorize.approve' | 'authorize.reject'
   | 'authorize.requests' | 'authorize.subscribe' | 'authorize.tab'
@@ -49,7 +49,7 @@ export interface MessageExtrinsicSignCancel {
   id: string
 }
 
-export type MessageExtrinsicSign = SignerPayload
+export type MessageExtrinsicSign = SignerPayloadJSON
 
 export interface MessageExtrinsicSignResponse {
   id: string
--- src/ts/page/Signer.ts
+++ src/ts/page/Signer.ts
@@ -2,8 +2,9 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { Signer as SignerInterface, SignerPayload, SignerResult } from '@polkadot/api/types'
+import { Signer as SignerInterface, SignerResult } from '@polkadot/api/types'
 import { SendRequest } from './types'
+import { SignerPayloadJSON } from '@polkadot/types/types'
 
 let sendRequest: SendRequest
 let nextId = 0
@@ -16,7 +17,7 @@ export default class Signer implements SignerInterface {
     sendRequest = _sendRequest
   }
 
-  public async signPayload (payload: SignerPayload): Promise<SignerResult> {
+  public async signPayload (payload: SignerPayloadJSON): Promise<SignerResult> {
     const id = ++nextId
     const result = await sendRequest('extrinsic.sign', payload)
 
