c2008d88b4584c129bdf48588aedd964cf5abed4
hskang9 <hskang9@gmail.com>
2020-01-19 16:42:01 -0800

    fix: rough timer ui transition

--- src/ts/constants/config.ts
+++ src/ts/constants/config.ts
@@ -1,1 +1,1 @@
-export const VALIDITY_INTERVAL = 300 // 5 minutes/300 seconds
+export const VALIDITY_INTERVAL = 301 // 5 minutes/300 seconds + 1 second for ui transition
--- src/ts/constants/utils.ts
+++ src/ts/constants/utils.ts
@@ -1,5 +1,7 @@
 export const parseTimeLeft = (timeleft) => {
+  if (timeleft === 301) return '05:00'
+  timeleft -= 1
   const minutesLeft = parseInt(`${timeleft / 60}`, 10)
   const secondsLeft = (timeleft % 60).toString()
-  return `0${minutesLeft}:${secondsLeft.length === 1 ? `0${secondsLeft}` : secondsLeft}`
+  return `0${minutesLeft}:${ secondsLeft.length === 1 ? `0${secondsLeft}` : secondsLeft}`
 }
