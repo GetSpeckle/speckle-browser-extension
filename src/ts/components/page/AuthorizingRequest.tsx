5c15b4655a314c9153090e0b3e93e2128e5c2a20
fyang1024 <fyang1024@gmail.com>
2020-05-02 10:47:35 +1000

    Add i18n for AuthorizingRequest.tsx

--- src/ts/components/page/AuthorizingRequest.tsx
+++ src/ts/components/page/AuthorizingRequest.tsx
@@ -7,6 +7,7 @@ import { Button } from '../basic-components'
 import AlertPanel from './AlertPanel'
 import { Grid, Button as BasicButton } from 'semantic-ui-react'
 import RequestPanel from './RequestPanel'
+import t from '../../services/i18n'
 
 const AuthorizingRequest = (props) => {
   const { id, request: { origin }, url } = props.request
@@ -21,7 +22,7 @@ const AuthorizingRequest = (props) => {
     <div>
 
       <div className='auth-description'>
-        {origin} is requesting access to your account
+        {origin} {t('authDesc')}
       </div>
 
       {/* TODO: use the real value */}
@@ -31,25 +32,25 @@ const AuthorizingRequest = (props) => {
 
       <AlertPanel
         color='red'
-        message={'The request is made from ' + url}
+        message={t('accessAlert') + url}
         icon='linkify'
       />
 
       <AlertPanel
         color='red'
-        message='Approving gives access to the address of your accounts'
+        message={t('approveAlert')}
         icon='exclamation'
       />
 
       <Grid columns='equal'>
         <Grid.Column>
           <BasicButton fluid={true} onClick={onReject} className='minor'>
-            Reject
+            {t('reject')}
           </BasicButton>
         </Grid.Column>
         <Grid.Column>
           <Button onClick={onApprove} className='narrow'>
-            Approve Access
+            {t('approve')}
           </Button>
         </Grid.Column>
       </Grid>
